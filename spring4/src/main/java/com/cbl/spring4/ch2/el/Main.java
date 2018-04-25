047d6c3cd9344c7fab45f2f79ee072eeb82eaeea
cuibl <cuibl@hjsoft.com.cn>
2018-04-26 00:20:54 +0800

    spring Spring EL和资源调用

---
+++ spring4/src/main/java/com/cbl/spring4/ch2/el/Main.java
@@ -0,0 +1,11 @@
+package com.cbl.spring4.ch2.el;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ElConfig.class);
+        ElConfig config = context.getBean(ElConfig.class);
+        config.outputResource();
+    }
+}
--- spring4/src/main/java/com/cbl/test/TestPush.java
+++
@@ -1,7 +0,0 @@
-package com.cbl.test;
-
-public class TestPush {
-    public static void main(String[] args) {
-        System.out.println("IDEA测试");
-    }
-}
